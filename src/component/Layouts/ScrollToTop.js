6747494f995c6646db65ecf42d52d21f0bb725cc
KateChu <chu.shihchien@gmail.com>
2022-08-28 15:58:24 +0800

    fix: scroll to top after path changed

--- src/component/Layouts/ScrollToTop.js
+++ src/component/Layouts/ScrollToTop.js
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
   const stateShowData = useSelector((state) => state.animal.showData);
+  const location = useLocation();
+  const path = location.pathname;
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [stateShowData]);
+  }, [stateShowData, path]);
 
   return null;
 }
